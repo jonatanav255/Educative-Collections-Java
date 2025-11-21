c3a0657fd8c90558c801745d6c02c779af3aac1b
Jonatan <jonatanav255@hotmail.com>
2025-11-20 23:24:57 -0600

    testing ArrayList, different method

--- collections/src/main/java/com/example/Main.java
+++ collections/src/main/java/com/example/Main.java
@@ -1,7 +1,20 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("3");
+        list.set(1, "4");
+
+        list.contains("1");
+
+        System.out.println(list);
+
     }
 }
